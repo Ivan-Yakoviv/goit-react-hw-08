b7e3e589cd89619fcfce843c497970014bbd892b
Ivan-Yakoviv <ivasykvv@gmail.com>
2024-08-20 21:56:46 +0300

    add persist

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,14 @@
 // import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h2>Greetings!</h2>
+      <h2>Here you can save the phone numbers of your contacts.</h2>
+      <h2>... but register first</h2>
+      <h2>...or login, if you already have account</h2>
+    </div>
+  );
 };
 
 export default Home;
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,29 @@ import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './contacts/slice';
 import { filtersReducer } from './filters/slice';
 import { authReducer } from './auth/slice';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore, FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE } from 'redux-persist';
 
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+  whitelist: ['token'],
+};
 
 export const store = configureStore({
     reducer: {
         contacts: contactsReducer,
         filters: filtersReducer,
-        auth: authReducer
+        auth: persistReducer(persistConfig, authReducer),
     },
+     middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
